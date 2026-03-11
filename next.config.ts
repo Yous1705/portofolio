93f0a5d4d01e41ae763d091ed65609accdaf8eea
Yous Sibarani <youssibarani17@gmail.com>
2026-03-12 00:56:16 +0700

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/portofolio",
-  assetPrefix: "/portofolio/",
   images: {
     unoptimized: true,
   },
